90fc7b7d5d6786ba0bb8ff01027ef44d46618c44
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 01:49:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,25 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->any;
+	$text = $json->result->parameters->text;
 
-	$speech = $text;
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
 
 	$response = new \stdClass();
 	$response->speech = $speech;
