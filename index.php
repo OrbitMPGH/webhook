9adb4800791092c6cf950a30a1d34da1debdf336
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 03:00:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include('Webhook.php');
 $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
-$wh->build_simpleResponse("Försöker lägga till " + $msg, "Försöker lägga till " + $msg);
+$wh->build_simpleResponse("Försöker lägga till ", "Försöker lägga till ");
 $wh->respond();
 
 
