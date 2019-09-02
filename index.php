726ffd9c7e565c2a5cd28875b37462c04cdc1919
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 02:39:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php 
 include('Webhook.php');
-$fp = fsockopen("0.tcp.eu.ngrok.io", 15756, $errno, $errstr, 30);
-fwrite($fp, "wazza");
-fclose($fp);
 $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
+if($msg == "dab")
+{
+	wow();
+}
 $wh->build_simpleResponse($msg, $msg);
 $wh->respond();
+
+function wow(){
+	$fp = fsockopen("0.tcp.eu.ngrok.io", 15756, $errno, $errstr, 30);
+	fwrite($fp, "queue");
+	fclose($fp);
+}
 
 ?>
