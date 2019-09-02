735cfa0f4a61fae11a16e0937c6e90e9c2d1720e
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 02:27:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 include('Webhook.php');
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($socket, "0.tcp.eu.ngrok.io", 15756)
+        or onSocketFailure("Failed to connect to 91.214.90.130:100", $socket);
+$args = ['projectId' => 'fancy-queue-e0980'];
+socket_write($socket, "wazza");
 $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
 $wh->build_simpleResponse($msg, $msg);
