c752a004e12e418ff9e24157e4b0108c7a37d4a7
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 02:23:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
 include('Webhook.php');
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($socket, "91.214.90.130", 100)
+        or onSocketFailure("Failed to connect to 91.214.90.130:100", $socket);
 $args = ['projectId' => 'fancy-queue-e0980'];
+socket_write($socket, "wazza");
 $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
 $wh->build_simpleResponse($msg, $msg);
