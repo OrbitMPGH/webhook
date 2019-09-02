9b783ff1e333236d112bd0f76b7b4e600a5ac6c8
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 02:11:56 +0200

    testar

--- index.php
+++ index.php
@@ -1,41 +1,8 @@
 <?php 
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
-if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
-	$json = json_decode($requestBody);
-
-	$text = $json->body->queryResult->parameters->{'text'};
-
-	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
-
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
-		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-	}
-
-	$response = new \stdClass();
-	$response->fulfillmentText->" "
-	$response->fulfillmentMessages->text->text->$speech
-	$response->source = "webhook";
-	echo json_encode($response);
-}
-else
-{
-	echo "Method not allowed";
-}
+include('Webhook.php');
+$args = ['projectId' => 'fancy-queue-e0980'];
+$wh = new Webhook($args);
+$wh->respond_simpleMessage('ta mitt liv');
+$wh->endConversation();
 
 ?>
