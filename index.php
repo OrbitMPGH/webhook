11e13fb9ed7ea30c691fe1d342e35abce2827ceb
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 02:51:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 include('Webhook.php');
 $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
-if($msg == "dab")
-{
-	wow();
-}
-$wh->build_simpleResponse($msg, $msg);
-$wh->respond();
+
 
 function wow(){
 	$fp = fsockopen("0.tcp.eu.ngrok.io", 15756, $errno, $errstr, 30);
-	fwrite($fp, "queue");
+	fwrite($fp, "search_queue" + $msg);
+	$line = socket_read($socket, 1024, PHP_NORMAL_READ);
+    if(substr($line, -1) === "\r") {
+        // read/skip one byte from the socket
+        // we assume that the next byte in the stream must be a \n.
+        // this is actually bad in practice; the script is vulnerable to unexpected values
+		socket_read($socket, 1, PHP_BINARY_READ);
+	}
+	$message = parseLine($line);
+	$wh->build_simpleResponse($message, $message);
+	$wh->respond();
 	fclose($fp);
 }
 
