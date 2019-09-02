353fd68a83a71167de66c70c2824cdb7bb0d2d8a
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 11:34:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,12 @@ $msg = $wh->get_parameter('text');
 $wh->build_simpleResponse("Försöker lägga till {$msg}", "Försöker lägga till {$msg}");
 $wh->endConversation();
 $wh->respond();
-$hasSent = false;
 wow($msg, $wh);
 
-function wow($songname, $hook){
-
-	if ($hasSent) return;
-	
-	$fp = fsockopen($serverip, 17796, $errno, $errstr, 30);
+function wow($songname, $hook){	
+	$fp = fsockopen("0.tcp.eu.ngrok.io", 17796, $errno, $errstr, 30);
 	fwrite($fp, utf8_encode("search_queue{$songname}"));
 	fclose($fp);
-	$hasSent = true;
-	return;
 }
 
 ?>
