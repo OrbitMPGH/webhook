6a0d568750f7f07707330e8d0021368579204d10
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 03:07:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
 $wh->build_simpleResponse("Försöker lägga till {$msg}", "Försöker lägga till {$msg}");
 $wh->respond();
-wow($msg, $wh);
-
-
+$hasSent = false;
 function wow($songname, $hook){
+
+	if ($hasSent) return;
 	
 	$fp = fsockopen("0.tcp.eu.ngrok.io", 15756, $errno, $errstr, 30);
 	fwrite($fp, "search_queue{$songname}");
-
 	fclose($fp);
+	$hasSent = true;
 }
 
 ?>
