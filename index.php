f8ca6d7079c96f87a484f5587b7a6139e681aff8
Orbit <OrbitMPGH@users.noreply.github.com>
2019-09-02 02:57:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 include('Webhook.php');
 $wh = new Webhook($args);
 $msg = $wh->get_parameter('text');
+$wh->build_simpleResponse("Försöker lägga till " + $msg, "Försöker lägga till " + $msg);
+$wh->respond();
 wow($msg, $wh);
 
 
@@ -9,16 +11,7 @@ function wow($songname, $hook){
 	
 	$fp = fsockopen("0.tcp.eu.ngrok.io", 15756, $errno, $errstr, 30);
 	fwrite($fp, "search_queue" + $songname);
-	$line = socket_read($socket, 1024, PHP_NORMAL_READ);
-    if(substr($line, -1) === "\r") {
-        // read/skip one byte from the socket
-        // we assume that the next byte in the stream must be a \n.
-        // this is actually bad in practice; the script is vulnerable to unexpected values
-		socket_read($socket, 1, PHP_BINARY_READ);
-	}
-	$message = parseLine($line);
-	$hook->build_simpleResponse($message, $message);
-	$hook->respond();
+
 	fclose($fp);
 }
 
